d97dfd66187713a9c874d73d67b5ed1393bc9ac8
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-08-16 00:19:42 +0530

    practice adding to do list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import InputTypeComponent from './components/InputTypeComponent';
 import Counter from './components/CounterComponent';
+import ToDoList from './components/ToDoListComponent';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
@@ -11,12 +12,14 @@ function App() {
         <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
         <Link to="/input-type" style={{ marginRight: "10px" }}>InputTypeComponent</Link>
         <Link to="/counter" style={{ marginRight: "10px" }}>Countersss</Link>
+        <Link to="/update-to-do" style={{ marginRight: "10px" }}>Update To Do List</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/input-type" element={<InputTypeComponent />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/update-to-do" element={<ToDoList />} />
       </Routes>
     </Router>
   );
---
+++ src/components/ToDoListComponent.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+function ToDoList(){
+    const [textData, setTextData] = useState("");
+    const [toDoList, setToDoList] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    //load from localstorage on when component mount
+    useEffect(()=>{
+        const storedToDos = JSON.parse(localStorage.getItem("toDos")) || [];
+        setToDoList(storedToDos);
+        setLoaded(true);
+    },[]);
+
+    //To save to the localstorage
+    useEffect(()=>{
+        if(loaded){
+        localStorage.setItem("toDos", JSON.stringify(toDoList));
+        }
+    }, [toDoList, loaded]);
+
+    const updateToDoList = () => {
+        if(textData==undefined || textData==null || textData.trim()==="") return;
+        if(toDoList.indexOf(textData.trim())!=-1) return;
+        setToDoList(toDoList => [...toDoList, textData]);
+        //setToDoList(toDoList.push(textData));
+        setTextData("");
+    }
+
+    const updateText = (event) =>{
+        setTextData(event.target.value);
+    }
+
+    const deleteItem = (indexToDelete) =>{
+        //setToDoList(toDoList => toDoList.splice(index,1));
+        setToDoList((prevList) => prevList.filter((_,i)=> i!==indexToDelete));
+    }
+
+    return(
+        <div>
+        <div>
+            <label>To Do List:</label>
+            <ul>
+                {
+                toDoList.map((data, index) =>(
+                    <li key={index}>{data} <button onClick={()=> deleteItem(index)}>Delete</button></li>
+                ))
+             }
+            </ul>
+        </div>
+        <div>
+            <input type="text" placeholder="Type here..." value={textData} onChange={(event)=> updateText(event)}></input>
+            <button onClick={()=>updateToDoList()}>Add</button>
+        </div>
+        </div>
+
+    );
+}
+
+export default ToDoList;
