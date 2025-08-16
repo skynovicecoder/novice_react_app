bd2a28450083ce6bed3e5728e55c2ffe92498d33
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-08-16 18:28:57 +0530

    adding focus functionality using ref and adding onKeyDown enter functionality

--- src/components/ToDoListComponent.js
+++ src/components/ToDoListComponent.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function ToDoList(){
     const [textData, setTextData] = useState("");
     const [toDoList, setToDoList] = useState([]);
     const [loaded, setLoaded] = useState(false);
+    const inputRef = useRef(null);
 
     //load from localstorage on when component mount
     useEffect(()=>{
         const storedToDos = JSON.parse(localStorage.getItem("toDos")) || [];
         setToDoList(storedToDos);
         setLoaded(true);
+        inputRef.current?.focus();  //UserREF + Focus tpgether used for dom component focus
     },[]);
 
     //To save to the localstorage
@@ -25,6 +27,7 @@ function ToDoList(){
         setToDoList(toDoList => [...toDoList, textData]);
         //setToDoList(toDoList.push(textData));
         setTextData("");
+        inputRef.current?.focus();
     }
 
     const updateText = (event) =>{
@@ -34,6 +37,13 @@ function ToDoList(){
     const deleteItem = (indexToDelete) =>{
         //setToDoList(toDoList => toDoList.splice(index,1));
         setToDoList((prevList) => prevList.filter((_,i)=> i!==indexToDelete));
+         inputRef.current?.focus(); 
+    }
+
+    const handleInputTypeKey = (e) => {
+        if(e.key==="Enter"){
+            updateToDoList();
+        }
     }
 
     return(
@@ -49,7 +59,7 @@ function ToDoList(){
             </ul>
         </div>
         <div>
-            <input type="text" placeholder="Type here..." value={textData} onChange={(event)=> updateText(event)}></input>
+            <input ref={inputRef} type="text" placeholder="Type here..." onKeyDown={(e)=> handleInputTypeKey(e)} value={textData} onChange={(event)=> updateText(event)}></input>
             <button onClick={()=>updateToDoList()}>Add</button>
         </div>
         </div>
